b520859e599a44228b09e0c03988bd39e1a96cbf
larm60 <kentaxu@yahoo.com>
2021-04-27 16:18:51 -0400

    script connects to AWS dynamodb and passes stationID, timestamp, and distance reading from mbed through serial to dynamodb

--- raspberrypi/main.py
+++ raspberrypi/main.py
@@ -1,26 +1,33 @@
 import sys, os
 import time
 import boto3
+import serial
 
-#Values for AWS credentials (change these based on user)
+#Values to access AWS
 AWS_ACCESS_KEY="AKIASXPIV3TYQ2WTEYCV"
 AWS_SECRET_ACCESS_KEY="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"
 AWS_REGION="us-east-1"
 DYNAMODB_TABLE='4180'
     
-#Accesses dynamodb table 
+#accesses dynamodb table on aws   
 dynamodb = boto3.resource('dynamodb', aws_access_key_id=AWS_ACCESS_KEY,
                           aws_secret_access_key=AWS_SECRET_ACCESS_KEY,
                           region_name =AWS_REGION)
 
 table = dynamodb.Table(DYNAMODB_TABLE)
 
-table.put_item(
-    Item={
-        "stationID": "mbed1",           #creates stationID parameter for where the distance reading is coming from
-        "timestamp": int(time.time()),  #creates timestamp for when the reading was made
-        "distance": 5                   #distance sensor reads (currently dummy number)
-        }
-    )
+while(True):
+    #gets value from mbed serial to pi
+    device = serial.Serial('/dev/ttyACM0')
+    data = int(str(device.readline())[2:-3])
 
-print("test")
+    #Sends sonar distance readings to dynamodb on AWS
+    table.put_item(
+        Item={
+            "stationID": "mbed1",           #sends mbed1 as stationID to dynamodb
+            "timestamp": int(time.time()),  #sends current time as timestamp
+            "distance (mm)": data           #sends sonar reading from mbed through serial to dynamodb
+            }
+        )
+
+    print("test")
