62febb1cb672080a4dcaf1247cfb4ca01b97b148
larm60 <kentaxu@yahoo.com>
2021-04-29 14:02:41 -0400

    Added threshold reading to dynamodb that can be updated

--- raspberrypi/main.py
+++ raspberrypi/main.py
@@ -19,15 +19,20 @@ table = dynamodb.Table(DYNAMODB_TABLE)
 while(True):
     #gets value from mbed serial to pi
     device = serial.Serial('/dev/ttyACM0')
-    data = int(str(device.readline())[2:-3])
+    data = str(device.readline())[2:-3]
+    temp = data.split(',')
 
-    #Sends sonar distance readings to dynamodb on AWS
-    table.put_item(
-        Item={
-            "stationID": "mbed1",           #sends mbed1 as stationID to dynamodb
-            "timestamp": int(time.time()),  #sends current time as timestamp
-            "distance (mm)": data           #sends sonar reading from mbed through serial to dynamodb
-            }
-        )
+    #Sends sonar distance/threshold readings to dynamodb on AWS
+    try:
+        table.put_item(
+            Item={
+                "stationID": "mbed1",           #sends mbed1 as stationID to dynamodb
+                "timestamp": int(time.time()),  #sends current time as timestamp
+                "distance": temp[0],           #sends sonar reading from mbed through serial to dynamodb
+                "threshold": temp[1]          #sends threshold value from mbed to dynamodb
+                }
+            )
+    except:
+        continue
 
     print("test")
