331b25e5fd60a5812be15c0722927db7569de756
larm60 <kentaxu@yahoo.com>
2021-04-28 23:45:58 -0400

    fixed bugs in keypad and added pushbutton to rearm once disarmed and made alarm only sound when intruder is nearby

--- mbed/main.cpp
+++ mbed/main.cpp
@@ -9,7 +9,9 @@ I2C i2c(p9, p10); // Setup the i2c bus on pins 28 and 27
 Mpr121 mpr121(&i2c, Mpr121::ADD_VSS); // Setup the Mpr121: constructor(i2c object, i2c address of the mpr121)
 PwmOut speaker(p22); // Setup speaker
 uLCD_4DGL uLCD(p28,p27,p29); // Setup uLCD
+Serial blue(p13, p14); //setup bluetooth
 PwmOut red(p21);
+DigitalIn button(p23);
 
 bool movement = false; //variables for states
 bool armed = true;
@@ -20,7 +22,7 @@ bool armed = true;
     //code to execute when the distance has changed
 //    printf("Distance %d mm\r\n", distance);
     printf("%d\n", distance);
-    if(distance <=100){
+    if(distance <=100 && armed){
         movement = true;
     }
 }
@@ -30,7 +32,7 @@ ultrasonic mu(p17, p18, .1, 1, &dist);    //Set the trigger pin to p6 and the ec
                                         //second, and call dist when the distance changes
 
 //For Testing purposes
-DigitalOut led1(LED2);
+DigitalOut led1(LED1);
 
 int hold0,hold1,hold2,hold3 = 0; //signal variables for correct password
 
@@ -48,27 +50,46 @@ void fallInterrupt(){
     //Section makes sure the pinpad takes the values 0486 in that order, but other values can still be in between
     if ((key_code ==0) && (hold0 == 0) && (hold1 == 0)&& (hold2 == 0) && (hold3 == 0)){ //checks to see if the first value is 0
         hold0 =1;}    
+        
     if ((key_code ==4) && (hold0 == 1) && (hold1 == 0)&& (hold2 == 0) && (hold3 == 0)){ //checks to see if the second value is 4
         hold1 =1;}
+    
     if ((key_code ==8) && (hold0 == 1) && (hold1 == 1)&& (hold2 == 0) && (hold3 == 0)){ //checks to see if the third value is 8
         hold2 =1;}
 
     if ((key_code ==6) && (hold0 == 1) && (hold1 == 1)&& (hold2 == 1) && (hold3 == 0)){ //checks to see if the lasst value is 6
         hold3 =1;}
-    
+        
     //Section makes sure that no other values are pressed in between 0486
     
-    if ((hold0 == 1) && (hold1 == 1)&& (hold2 == 1) && (hold3 == 1)) {
+    if (key_code == 1 || key_code == 2 || key_code == 3 || key_code == 5 || key_code == 7 || key_code == 9 || key_code == 10 || key_code == 11 ) {
+        hold0 = 0;
+        hold1 = 0;
+        hold2 = 0;
+    }
+    
+     if ((hold0 == 1) && (hold1 == 1)&& (hold2 == 1) && (hold3 == 1) && armed) {
         led1 = 1; 
         armed = false;
+        movement = false; 
         speaker.period(1.0/523.25);
+        speaker = 0.5;
         wait(.2);
+        speaker = 0;
         speaker.period(1.0/659.25);
+        speaker = 0.5;
         wait(.2);
+        speaker = 0;
         speaker.period(1.0/783.99);
+        speaker = 0.5;
         wait(.2);
-
-    }
+        speaker = 0;
+        hold0 = 0;
+        hold1 = 0;
+        hold2 = 0;
+        hold3 = 0;
+    } 
+    
 }
 
 void Speaker_Function() {
@@ -80,8 +101,11 @@ void Speaker_Function() {
             speaker = float(i)/50.0;
             wait(.25);
             speaker.period(1.0/800.0);
+            speaker = float(i)/50.0;
+            wait(.15);
             //wait(0.5);;
-        }    
+        }   
+        movement = false;     //this will make the alarm only beep when someone is nearby
     } else {
         speaker = 0;
     }
@@ -103,24 +127,41 @@ void red_led() {
 void LCD_status() {
     if (!armed) {
         uLCD.text_string("DISARMED",1,14, FONT_7X8, 0x00FF00); //green font
-        uLCD.text_string("DISARMED",1,14, FONT_7X8, 0x000000); //black font
+        //uLCD.text_string("DISARMED",1,14, FONT_7X8, 0x000000); //black font
     } else if (armed && !movement) {
-        uLCD.text_string("ARMED - STAY AWAY",1,14, FONT_7X8, 0xFFFFFF); //white font
-        uLCD.text_string("ARMED - STAY AWAY",1,14, FONT_7X8, 0x000000); //black font
+        uLCD.text_string("ARMED",1,14, FONT_7X8, 0xFFFFFF); //white font
+        //uLCD.text_string("ARMED - STAY AWAY",1,14, FONT_7X8, 0x000000); //black font
     } else {
         uLCD.text_string("INTRUDER",1,14, FONT_7X8, 0xFF0000); //red font
-        uLCD.text_string("INTRUDER",1,14, FONT_7X8, 0x000000); //black font
+        //uLCD.text_string("INTRUDER",1,14, FONT_7X8, 0x000000); //black font
     }
 }
 
+//void Bluetooth() {
+//    if (blue.getc()=='!') {
+//        if (blue.getc()=='B') { //button data
+//            char bnum = blue.getc(); //button number
+//            if (blue.getc() == '1') { //on button press
+//                if (bnum=='1') { //arms when button 1 is pressed
+//                    armed = true;
+//                } else if (bnum == '2') { //disarms when button 2 is pressed
+//                    armed = false;
+//                }
+//            }
+//        }
+//    }
+//}
+
 
 int main() {
-    interrupt.fall(&fallInterrupt);
-    interrupt.mode(PullUp);
+    //interrupt.fall(&fallInterrupt);
+    //interrupt.mode(PullUp);
+    button.mode(PullUp);
     mu.startUpdates();//start measuring the distance
     while(1)
     {
-        //Do something else here
+        fallInterrupt();
+        
         mu.checkDistance();     //call checkDistance() as much as possible, as this is where
                                 //the class checks if dist needs to be called.
         Speaker_Function();
@@ -128,5 +169,15 @@ int main() {
         LCD_status();
         
         red_led();
+        
+        if(!button && !armed) {   //if button pressed, rearms
+            armed = true;
+            movement = false; 
+            hold0 = 0;
+            hold1 = 0;
+            hold2 = 0;
+            hold3 = 0;
+            uLCD.text_string("DISARMED",1,14, FONT_7X8, 0x000000); //black font
+        }
     }
 }
